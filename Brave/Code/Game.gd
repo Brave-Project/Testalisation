94aead470a7e486bb793702fc6d3298860c84d8e
Woodlutins <82327136+Woodlutins@users.noreply.github.com>
2021-04-24 15:26:35 +0200

    Delete Brave directory

--- Brave/Code/Game.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-# Declare member variables here. Examples:
-func _ready():
-	var Scene=load("res://Scene/Player.tscn")
-	var Play = Scene.instance()
-	add_child(Play)
-	pass # Replace with function body.

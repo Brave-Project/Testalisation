f182d2372860a81e28012c987e4fc0775084b14e
Woodlutins <poissonsansfish@gmail.com>
2021-05-01 15:26:10 +0200

    Tourelle fonctionnel

--- Brave/Code/Tourelle.gd
+++ Brave/Code/Tourelle.gd
@@ -1,17 +1,31 @@
 extends Area2D
 var Balle = preload("res://Scene/BalleTourelle.tscn")
+var tir=0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _on_Timer_timeout():
-	var Bal=Balle.instance()
-	get_tree().get_root().add_child(Bal)
-	Bal.global_position=global_position
+	if tir==1:
+		var Bal=Balle.instance()
+		get_tree().get_root().add_child(Bal)
+		Bal.start(global_transform)
+	pass # Replace with function body.
+
+
+func _on_Tourelle_area_entered(area):
+	if area.name=="Bullet":
+		area.queue_free()
+		queue_free()
+		VarGlobal.Score_Plus(1000)
+	pass # Replace with function body.
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_VisibilityNotifier2D_screen_entered():
+	tir=1
 	pass # Replace with function body.
